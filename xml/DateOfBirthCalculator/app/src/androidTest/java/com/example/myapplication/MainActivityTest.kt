35054eb641310b6d22041f4feac2fd564effec56
ondrapa <ondrej.pavelka@juicymo.cz>
2022-12-26 12:19:56 +0100

    Setup

---
+++ xml/DateOfBirthCalculator/app/src/androidTest/java/com/example/myapplication/MainActivityTest.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class MainActivityTest {
+
+    @Test
+    fun test_isActivityInView() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+    }
+}
